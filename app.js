90c42a271123780abc9746bca528443611d072ed
guAnsunyata <guansunyata@gmail.com>
2016-05-08 23:29:08 +0800

    fix api name

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('/api/activity/create', function (req, res){
 	});
 });
 
-app.post('/api/activity/', function (req, res){
+app.post('/api/activity/createRecord', function (req, res){
 	var activity = new activityAPI({});
 	activity.createUser(req.body._id, req.body.userName, req.body.events);
 });
