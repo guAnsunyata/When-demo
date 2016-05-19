2e4b84fb4fc0d1c7891b3c2cd858b1765c934932
guAnsunyata <guansunyata@gmail.com>
2016-05-19 09:42:19 +0800

    fix css request bug

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/api/activity/create', function (req, res){
 
 app.post('/api/activity/createRecord', function (req, res){
 	var activity = new activityAPI({});
-	activity.createUser(req.body._id, req.body.userName, req.body.events);
+	activity.createUser(req.body.id, req.body.userName, req.body.events);
 	res.json('success');
 });
 
