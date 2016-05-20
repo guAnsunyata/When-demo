9e3d807ad164238b8ab3141f2460adc2f9d48b81
guAnsunyata <guansunyata@gmail.com>
2016-05-21 02:35:10 +0800

    fixed fb secret

--- passport/auth.js
+++ passport/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'facebookAuth' : {
 		'clientID': '633348193500705',
-		'clientSecret': '0fdffb0059d2d5be12e1b45553409dc2',
+		'clientSecret': 'd3192aecd3688057c954ea740d112641',
 		'callbackURL': 'https://yuntech-when.herokuapp.com/auth/facebook/callback'
 	}
 }
